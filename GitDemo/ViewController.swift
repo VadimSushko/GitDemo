d716eae1bb3aa248772dbcbb3d94fd19a57d264b
Vadim Sushko <vadim.sushko@gmail.com>
2020-09-11 20:27:22 +0300

    initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Vadym Sushko on 11.09.2020.
+//  Copyright © 2020 Vadym Sushko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
